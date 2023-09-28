56a56e9bc976d204229e90d3c849ac2e55bc0110
darren <darrenxychen@gmail.com>
2023-09-28 16:08:20 -0700

    putting pseudo into code

--- project1.py
+++ project1.py
@@ -4,4 +4,15 @@
 # Project 1 CPSC 335
 
 def Solution(Busy_schedule: list[int], Working_period: list[int], duration: int):
-  pass
+  availability = []
+
+  Busy_schedule2 = []
+
+  #get minimum working time
+  start_time = max(Busy_schedule[0], Busy_schedule2[0])
+  end_time = min(Busy_schedule[Busy_schedule.length()-1], Busy_schedule2[Busy_schedule2.length()-1])
+
+  #get the baseline schedule
+  baseschedule = [start_time, end_time]
+
+# Solution([['12:00':'13:00'],['14:00':'15:00']], ['9:00','19:00'], 30)
