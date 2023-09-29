b9f4666157b1396653456d4b1600c24defd283fc
darren <darrenxychen@gmail.com>
2023-09-28 21:47:52 -0700

    trying to add the text file parser

--- project1.py
+++ project1.py
@@ -6,6 +6,27 @@
 schedule = [[1], [2]]
 # schedule = [['12:00':'13:00'],['14:00':'15:00']]
 
+# read file
+
+def main():
+  file = open('input.txt', 'r')
+  read = file.readlines()
+  file.close()
+
+  duration = 0
+  n = 1
+
+  for line in read:
+    line = line.strip()
+
+    #sets the duration
+    if line.isdigit():  # Check if the line is a valid integer
+      duration = int(line)
+  print(duration)
+
+
+
+
 def Solution(Busy_schedule: list[int], Working_period: list[int], duration: int):
   availability = []
 
@@ -20,4 +41,24 @@ def Solution(Busy_schedule: list[int], Working_period: list[int], duration: int)
 
   print(baseschedule)
 
-Solution(schedule, ['9:00','19:00'], 30)
+  #get the busy times
+  busy_times = Busy_schedule + Busy_schedule2
+
+  #trim the busy times off the baseline schedule
+  for i in busy_times:
+    for interval in busy_times:
+      if interval > start_time and interval < end_time:
+        s, e = busy_times.remove(interval)
+        availability.append([s, i.s])
+        availability.append([e, i.e])
+      #set the interval now to e since we have to trim off the first part
+      if interval < start_time:
+        interval.s = i.e
+      #reverse it
+      if interval > end_time:
+        interval.e = i.s
+
+  return availability
+
+main()
+# Solution(schedule, ['9:00','19:00'], 30)
