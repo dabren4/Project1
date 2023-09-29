ed99aea21136b8da341faa566a63670f49e92b66
darren <darrenxychen@gmail.com>
2023-09-29 15:50:58 -0700

    updates

--- project1.py
+++ project1.py
@@ -11,50 +11,49 @@ schedule = [[1], [2]]
 
 import re
 
-def main():
-  with open('input.txt') as f:
-    lines = f.readlines()
 
-  i = 0
-  while i < len(lines):
-    busy_schedule1 = re.findall(r"'(\d+:\d+)':'(\d+:\d+)'", lines[i])
-    busy_schedule1 = [[start, end] for start, end in busy_schedule1]
+with open('input.txt') as f:
+  lines = f.readlines()
 
-    i += 1
+i = 0
+while i < len(lines):
+  busy_schedule1 = re.findall(r"'(\d+:\d+)':'(\d+:\d+)'", lines[i])
+  busy_schedule1 = [[start, end] for start, end in busy_schedule1]
 
-    # Parse working period as list instead of string
-    working_period1 = lines[i].strip()[2:-2].split("','")
+  i += 1
 
-    i += 1
+  # Parse working period as list instead of string
+  working_period1 = lines[i].strip()[2:-2].split("','")
 
-    busy_schedule2 = re.findall(r"'(\d+:\d+)':'(\d+:\d+)'", lines[i])
-    busy_schedule2 = [[start, end] for start, end in busy_schedule2]
+  i += 1
 
-    i += 1
+  busy_schedule2 = re.findall(r"'(\d+:\d+)':'(\d+:\d+)'", lines[i])
+  busy_schedule2 = [[start, end] for start, end in busy_schedule2]
 
-    working_period2 = lines[i].strip()[2:-2].split("','")
+  i += 1
 
-    i += 1
+  working_period2 = lines[i].strip()[2:-2].split("','")
 
-    duration = int(lines[i])
+  i += 1
 
-    i += 1
+  duration = int(lines[i])
 
-    # Do something with the extracted info
-
-    print(busy_schedule1)
-    print(working_period1)
-    print(busy_schedule2)
-    print(working_period2)
-    print(duration)
+  i += 1
 
 
+  #print to check
+  print(busy_schedule1)
+  print(working_period1)
+  print(busy_schedule2)
+  print(working_period2)
+  print(duration)
+  print(" ")
 
 
-def Solution(Busy_schedule: list[int], Working_period: list[int], duration: int):
+
+
+def Solution(Busy_schedule, Working_period, Busy_schedule2, Working_period2, duration):
   availability = []
-
-  Busy_schedule2 = [[5], [7]]
 
   #get minimum working time
   start_time = max(Busy_schedule[0], Busy_schedule2[0])
@@ -84,5 +83,4 @@ def Solution(Busy_schedule: list[int], Working_period: list[int], duration: int)
 
   return availability
 
-main()
-# Solution(schedule, ['9:00','19:00'], 30)
+Solution(busy_schedule1, working_period1, busy_schedule2, working_period2, duration)
